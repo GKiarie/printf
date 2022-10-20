7e22c24dbf5d199df4f96da366416a407bb80617
Isaac Gitau <igitaukiarie@gmail.com>
2022-10-20 13:02:33 -0700

    rot13 prototype to header

--- main.h
+++ main.h
@@ -41,5 +41,6 @@ int print_X(va_list parameter_args);
 int print_S(va_list parameter_args);
 int print_p(va_list parameter_args);
 int print_pow(va_list parameter_args);
+int print_rot13(va_list parameter_args);
 
 #endif
