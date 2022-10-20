fefeadb7e7fdc26070b1ac6f74baed1d63fab34d
CABON-TECH <kishushubonifacembogho@gmail.com>
2022-10-20 12:06:39 -0700

    print string

--- print_string.c
+++ print_string.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+/**
  * print_S - prints a string and nonprintable character ascii values
  * @S: string to print
  * Return: number of chars printed
@@ -8,7 +30,7 @@ int print_S(va_list S)
 {
 	unsigned int i;
 	int count = 0;
-	char s = va_arg(S, char *);
+	char *str = va_arg(S, char *);
 
 	if (str == NULL)
 		str = "(null)";
